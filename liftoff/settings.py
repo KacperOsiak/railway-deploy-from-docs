d6ea4d14dfc3b4eb3c3a53fb29070985fc38e22a
KacperOsiak <kacperosiak77@gmail.com>
2026-07-07 19:35:27 +0200

    CSRF_TRUSTED_ORIGINS for RAILWAY

--- liftoff/settings.py
+++ liftoff/settings.py
@@ -28,7 +28,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-
+CSRF_TRUSTED_ORIGINS = [
+    'https://railway-deploy-from-docs-production.up.railway.app',
+]
 # Application definition
 
 INSTALLED_APPS = [
